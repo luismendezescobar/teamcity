20f43b935ba3e2fd961e1d6d2ea4dabd67d48137
luis.mendez <luismendeze@hotmail.com>
2022-11-21 18:25:19 -0600

    fixing compile error

--- java/src/main/java/com/infojolt/App.java
+++ java/src/main/java/com/infojolt/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Pluralsight luis);
+        System.out.println( "Hello Pluralsight luis");
     }
 }
